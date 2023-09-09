42b3e5faaaa5beeccc1d9a7de2dfa19753ca9b63
ilya valevin <ilyavalevin@mail.ru>
2023-09-09 11:16:36 +0300

    что то похожее на итог

--- calculator.js
+++ calculator.js
@@ -1,85 +1,84 @@
+    var display = document.querySelector('.input_display');
+    var numbers = document.querySelectorAll('.numbers_btn');
+    var operators = document.querySelectorAll('.operator_btn');
+    var clearButton = document.querySelector('.clear');
+    var equalButton = document.querySelector('.equal_btn');
 
-const display = document.querySelector('.input_display');
-const numbers = document.querySelectorAll('.numbers_btn');
-const operators = document.querySelectorAll('.operator_btn');
-const clearButton = document.querySelector('.clear');
-const equalButton = document.querySelector('.equal_btn');
 
+    var currentInput = '';
+    var operator = '';
+    var prevInput = '';
 
-var currentInput = '';
-var operator = '';
-var prevInput = '';
-
-for (var i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener('click', function () {
-    currentInput += this.textContent;
-    updateDisplay();
-  });
-}
-
-for (var j = 0; j < operators.length; j++) {
-  operators[j].addEventListener('click', function () {
-    if (prevInput !== '' && currentInput !== '') {
-      calculate();
+    for (var i = 0; i < numbers.length; i++) {
+      numbers[i].addEventListener('click', function () {
+        currentInput += this.textContent;
+        updateDisplay();
+      });
     }
-    operator = this.textContent;
-    prevInput = currentInput;
-    currentInput = '';
-    updateDisplay();
-  });
-}
+
+    for (var j = 0; j < operators.length; j++) {
+      operators[j].addEventListener('click', function () {
+        if (prevInput !== '' && currentInput !== '') {
+          calculate();
+        }
+        operator = this.textContent;
+        prevInput = currentInput;
+        currentInput = '';
+        updateDisplay();
+      });
+    }
+
+    function clear() {
+      currentInput = '';
+      operator = '';
+      prevInput = '';
+    }
+
+    function updateDisplay() {
+      display.textContent = currentInput || prevInput || '0';
+    }
 
 
-clearButton.addEventListener('click', function () {
-  clear();
-  updateDisplay();
-});
+    clearButton.addEventListener('click', function () {
+      clear();
+      updateDisplay();
+    });
+
+    equalButton.addEventListener('click', function () {
+      calculate();
+      updateDisplay();
+    });
 
 
-equalButton.addEventListener('click', function () {
-  calculate();
-  updateDisplay();
-});
+    function calculate() {
+      var prevNum = parseFloat(prevInput);
+      var currentNum = parseFloat(currentInput);
 
-
-function updateDisplay() {
-  display.textContent = currentInput || prevInput || '0';
-}
-
-
-function calculate() {
-  var prevNum = parseFloat(prevInput);
-  var currentNum = parseFloat(currentInput);
-
-  switch (operator) {
-    case '+':
-      currentInput = String(prevNum + currentNum);
-      break;
-    case '-':
-      currentInput = String(prevNum - currentNum);
-      break;
-    case '*':
-      currentInput = String(prevNum * currentNum);
-      break;
-    case '/':
-      if (currentNum === 0) {
-        currentInput = 'Ошибка';
-      } else {
-        currentInput = String(prevNum / currentNum);
+      switch (operator) {
+        case '+':
+          currentInput = String(prevNum + currentNum);
+          break;
+        case '-':
+          currentInput = String(prevNum - currentNum);
+          break;
+        case '*':
+          currentInput = String(prevNum * currentNum);
+          break;
+        case '/':
+          if (currentNum === 0) {
+            currentInput = 'Ошибка';
+          } else {
+            currentInput = String(prevNum / currentNum);
+          }
+          break;
+        default:
+          break;
       }
-      break;
-    default:
-      break;
-  }
 
-  operator = '';
-  prevInput = currentInput;
-  currentInput = '';
-}
+      operator = '';
+      prevInput = currentInput;
+      currentInput = '';
+    }
 
-// Функция для очистки
-function clear() {
-  currentInput = '';
-  operator = '';
-  prevInput = '';
-}
+
+
--- math.js
+++ math.js
@@ -10,3 +10,30 @@ function mult(a,b){
 function devide(a,b){
   return a/b;
 }
+
+
+function calculate() {
+  var prevNum = parseFloat(prevInput);
+  var currentNum = parseFloat(currentInput);
+
+  switch (operator) {
+    case '+':
+      currentInput = String(prevNum + currentNum);
+      break;
+    case '-':
+      currentInput = String(prevNum - currentNum);
+      break;
+    case '*':
+      currentInput = String(prevNum * currentNum);
+      break;
+    case '/':
+      if (currentNum === 0) {
+        currentInput = 'Ошибка';
+      } else {
+        currentInput = String(prevNum / currentNum);
+      }
+      break;
+    default:
+      break;
+  }
+}
